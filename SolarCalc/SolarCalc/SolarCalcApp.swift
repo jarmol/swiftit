7e126c4bbece7c1c766438ee016dd9146c6bc692
Polarit <jarmol@users.noreply.github.com>
2025-10-21 13:09:15 +0300

    Create SolarCalcApp.swift

---
+++ SolarCalc/SolarCalc/SolarCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolarCalcApp.swift
+//  SolarCalc
+//
+//  Created by Jarmo Lammi on 21.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct SolarCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
